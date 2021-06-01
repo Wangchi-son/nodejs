b641246baa5781e33a4e6d6ef2163f91af07cf17
DESKTOP-T7HINV9\sonwonjae <sonwj0915@naver.com>
2021-06-02 00:11:35 +0900

    post방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body) {
     <body>
     <h1><a href="/">WEB2</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
     </body>
     </html>
@@ -38,7 +40,6 @@ var app = http.createServer(function (request, response) {
   if (pathname === "/") {
     if (queryData.get("id") === null) {
       fs.readdir("./data", (err, fileList) => {
-        console.log(fileList);
         var title = "Welcome";
         var description = "Hello, Node.js";
         var list = templateList(fileList);
@@ -70,6 +71,38 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", (err, fileList) => {
+      var title = "WEB - create";
+      var list = templateList(fileList);
+
+      var template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/create_process" method="POST">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="description" placeholder="description" ></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    request.on("data", (data) => {
+      body = body + data;
+    });
+    request.on("end", () => {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      console.log(post.title);
+    });
+    response.writeHead(200);
+    response.end("success");
   } else {
     response.writeHead(404);
     response.end("Not found");
