c9b96e0e89e21b5d0e6b74496d9d0b1bae906840
DESKTOP-T7HINV9\sonwonjae <sonwj0915@naver.com>
2021-06-01 18:44:34 +0900

    query string에 따라 다른정보를 출력하는 코드

---
+++ syntax/string.js
@@ -0,0 +1,5 @@
+console.log("1" + "1");
+console.log(
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero ab mollitia sed aliquid doloribus itaque, eos vitae consequuntur corrupti, quia provident molestiae pariatur aperiam assumenda quis quo possimus autem quod?"
+    .length
+);
