bbbedfe5c98e912fe8a35e57e398c0fd2560c76d
DESKTOP-T7HINV9\sonwonjae <sonwj0915@naver.com>
2021-06-01 20:31:40 +0900

    홈페이지 구현

--- main.js
+++ main.js
@@ -6,34 +6,59 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = new URL("http://localhost:3000" + _url).searchParams;
   var pathname = new URL("http://localhost:3000" + _url).pathname;
-  var title = queryData.get("id");
 
   console.log(new URL("http://localhost:3000" + _url));
   if (pathname === "/") {
-    fs.readFile(`data/${queryData.get("id")}`, "utf8", (err, description) => {
+    if (queryData.get("id") === null) {
+      var title = "Welcome";
+      var description = "Hello, Node.js";
       var template = `
-    <!doctype html>
-    <html>
-    <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${description}</p>
-    </body>
-    </html>
-
-  `;
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+        </body>
+        </html>
+        `;
       response.writeHead(200);
       response.end(template);
-    });
+    } else {
+      fs.readFile(`data/${queryData.get("id")}`, "utf8", (err, description) => {
+        var title = queryData.get("id");
+        var template = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+            </body>
+            </html>
+            `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
