a12530e7325e8862f18927fa7210906bed811ebd
DESKTOP-T7HINV9\sonwonjae <sonwj0915@naver.com>
2021-06-01 20:50:21 +0900

    글목록 출력하기

--- main.js
+++ main.js
@@ -10,32 +10,21 @@ var app = http.createServer(function (request, response) {
   console.log(new URL("http://localhost:3000" + _url));
   if (pathname === "/") {
     if (queryData.get("id") === null) {
-      var title = "Welcome";
-      var description = "Hello, Node.js";
-      var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-        `;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`data/${queryData.get("id")}`, "utf8", (err, description) => {
-        var title = queryData.get("id");
+      fs.readdir("./data", (err, fileList) => {
+        console.log(fileList);
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+
+        // 게시글 리스트 출력 파트
+        var i = 0;
+        while (i < fileList.length) {
+          list =
+            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+
         var template = `
             <!doctype html>
             <html>
@@ -45,11 +34,7 @@ var app = http.createServer(function (request, response) {
             </head>
             <body>
             <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
             </body>
@@ -57,6 +42,46 @@ var app = http.createServer(function (request, response) {
             `;
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir("./data", (err, fileList) => {
+        console.log(fileList);
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = "<ul>";
+
+        // 게시글 리스트 출력 파트
+        var i = 0;
+        while (i < fileList.length) {
+          list =
+            list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+        fs.readFile(
+          `data/${queryData.get("id")}`,
+          "utf8",
+          (err, description) => {
+            var title = queryData.get("id");
+            var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
       });
     }
   } else {
