5c143da9b9e079e98f023f8a31e63e9a6fb4e255
DESKTOP-T7HINV9\sonwonjae <sonwj0915@naver.com>
2021-06-03 12:56:06 +0900

    출력정보에 대한 보안

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var url = require("url");
 var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -30,15 +31,19 @@ var app = http.createServer(function (request, response) {
         var filteredId = path.parse(queryData.get("id")).base;
         fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
           var title = queryData.get("id");
+          var sanitizedTitle = sanitizeHtml(title);
+          var sanitizedDescription = sanitizeHtml(description, {
+            allowedTags: ["h1"],
+          });
           var list = template.list(fileList);
           var html = template.HTML(
-            title,
+            sanitizedTitle,
             list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             ` <a href="/create">create</a>
-                <a href="/update?id=${title}">update</a>
+                <a href="/update?id=${sanitizedTitle}">update</a>
                 <form action="delete_process" method="post" onsubmit="">
-                  <input type="hidden" name="id" value="${title}">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>
               `
